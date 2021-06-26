856068a16dfc1237abfba21bb2c1372f8e5a20a2
JanuaryThomas <january_thomas@hotmail.com>
2021-06-26 07:28:47 +0300

    adding second step

--- aws-step-function/lambdas/stateA/app.py
+++ aws-step-function/lambdas/stateA/app.py
@@ -1,3 +1,4 @@
 
 def handler(event: dict, context: dict) -> dict:
+    message = event.get('message')
     return 'A'
---
+++ aws-step-function/lambdas/stateB/app.py
@@ -0,0 +1,4 @@
+
+def handler(event: dict, context: dict) -> dict:
+    print(event)
+    return 'A'
